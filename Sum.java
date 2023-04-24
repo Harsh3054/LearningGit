e463f5c612aaf8c1e25a540f3955841073a66ab2
Harshal Sandip Patil <harshalspatil21@gmail.com>
2023-04-24 10:44:56 +0530

    added message in Sum.java

--- Sum.java
+++ Sum.java
@@ -1,9 +1,8 @@
 
-
 public class Sum{
     public static void main(String[] args) {
         int a=7;
         int b=6;
-        System.out.println(a+b);
+        System.out.println("The sum is"+" "+a+b);
     }
 }
